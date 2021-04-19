5c8c7b2f31b5c0bc967ca649e5f5cb976b3eaf97
Rayhan <rayhan@badr.co.id>
2021-04-20 00:19:21 +0700

    update footer

--- components/footer.php
+++ components/footer.php
@@ -1,23 +1,27 @@
-<footer>
-    <div class="row no-gutters bg-dark">
-        <div class="mb-3 mx-auto"></div>
-        <div class="d-md-flex justify-content-around bd-highlight col-md-12 pt-5 pb-5 mb-3">
-            <div class="p-2 flex-fill bd-highlight mb-5 mb-md-0">
-                <h3 class="text-white">A.Rayhan.P</h3> <small class="text-white">Copyright &copy 2019</small>
+<footer class="bg-dark">
+    <div class="container">
+        <div class="row no-gutters">
+            <div class="mb-3"></div>
+            <div class="d-md-flex justify-content-between bd-highlight col-md-12 pt-5 pb-5 mb-3">
+                <div class="bd-highlight mb-5 mb-md-0">
+                    <h3 class="text-white">A.Rayhan.P</h3> <small class="text-white">Copyright &copy 2021</small>
+                </div>
+                <div class="d-md-flex">
+                    <div class="px-md-4 flex-fill bd-highlight mb-3 mb-md-0">
+                        <h5 class="text-white">Instagram</h5>
+                        <a class="mb-0" href="https://instagram.com/arayhan_" target="_blank">arayhan_</a>
+                    </div>
+                    <div class="px-md-4 flex-fill bd-highlight mb-3 mb-md-0">
+                        <h5 class="text-white">Website</h5>
+                        <a class="mb-0" href="https://arayhan.com" target="_blank">arayhan.com</a>
+                    </div>
+                    <div class="px-md-4 flex-fill bd-highlight mb-3 mb-md-0">
+                        <h5 class="text-white">Linkedin</h5>
+                        <a class="mb-0" href="https://www.linkedin.com/in/arayhan/" target="_blank">arayhan</a>
+                    </div>
+                </div>
             </div>
-            <div class="p-2 flex-fill bd-highlight mb-3 mb-md-0">
-                <h5 class="text-white">Instagram</h5>
-                <a class="mb-0" href="https://instagram.com/arayhan_" target="_blank">arayhan_</a>
-            </div>
-            <div class="p-2 flex-fill bd-highlight mb-3 mb-md-0">
-                <h5 class="text-white">Website</h5>
-                <a class="mb-0" href="https://arayhan.com" target="_blank">arayhan.com</a>
-            </div>
-            <div class="p-2 flex-fill bd-highlight mb-3 mb-md-0">
-                <h5 class="text-white">Linkedin</h5>
-                <a class="mb-0" href="https://www.linkedin.com/in/arayhan/" target="_blank">arayhan</a>
-            </div>
+            <div class="line mb-3 mx-auto"></div>
         </div>
-        <div class="line mb-3 mx-auto"></div>
     </div>
 </footer>
