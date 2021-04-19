ff61b8643e5df7e00aae07bfb6537670eb056b5a
Rayhan <rayhan@badr.co.id>
2021-04-20 00:19:37 +0700

    update menu container

--- components/menu.php
+++ components/menu.php
@@ -1,6 +1,6 @@
 <div>
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-        <div class="container-fluid">
+        <div class="container">
             <a class="navbar-brand" href="#">A.Rayhan.P</a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
