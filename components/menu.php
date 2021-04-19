ce58bb16a01230d39b9cc015d14456a170ebd840
Rayhan <rayhan@badr.co.id>
2021-04-20 00:06:03 +0700

    update menu

--- components/menu.php
+++ components/menu.php
@@ -24,9 +24,25 @@
                     <li class="nav-item">
                         <a class="nav-link d-lg-none" href="#">Login</a>
                     </li>
+                    <!-- <li class="nav-item dropdown d-lg-none">
+                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            Ahmed Rayhan Primadedas
+                        </a>
+                        <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                            <a class="dropdown-item" href="#">Logout</a>
+                        </div>
+                    </li> -->
                 </ul>
             </div>
             <a href="#" class="btn btn-primary d-none d-lg-block">Login</a>
+            <!-- <div class="dropdown d-none d-lg-block">
+                <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    Ahmed Rayhan Primadedas
+                </button>
+                <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                    <a class="dropdown-item" href="#">Logout</a>
+                </div>
+            </div> -->
         </div>
     </nav>
 </div>
