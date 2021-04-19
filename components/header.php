8bc8fef623c4f87fe654194c0c5ff2847902a41f
Rayhan <rayhan@badr.co.id>
2021-04-19 23:52:10 +0700

    fix banner

--- components/header.php
+++ components/header.php
@@ -6,7 +6,7 @@
     </ol>
     <div class="carousel-inner">
         <div class="carousel-item active">
-            <div class="jumbotron">
+            <div class="jumbotron mb-0">
                 <div class="container">
                     <h1 class="display-4 text-white">Selamat Datang!</h1>
                     <p class="lead text-white">Ahmed Rayhan Primadedas - 0110219083 - TI 03</p>
@@ -16,7 +16,7 @@
             </div>
         </div>
         <div class="carousel-item">
-            <div class="jumbotron">
+            <div class="jumbotron mb-0">
                 <div class="container">
                     <h1 class="display-4 text-white">Tugas UTS PWL</h1>
                     <p class="lead text-white">Ahmed Rayhan Primadedas - 0110219083 - TI 03</p>
