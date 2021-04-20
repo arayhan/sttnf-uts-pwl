d022ec9576b4a4441078dcb942d6c3df722e73aa
Rayhan <rayhan@badr.co.id>
2021-04-20 19:40:51 +0700

    handle role action permission on data pegawai

--- data-pegawai.php
+++ data-pegawai.php
@@ -34,11 +34,17 @@ $result = $pegawai->getPegawai();
                 <td><?= $data['divisi']; ?></td>
                 <td>
                     <a href="index.php?page=detail-pegawai&id=<?= $data['id'] ?>" class="badge badge-success">Detail</a>
-                    <a href="index.php?page=form-edit-pegawai&id=<?= $data['id'] ?>" class="badge badge-warning">Edit</a>
-                    <form method="post" action="controllers/pegawaiController.php">
-                        <input type="hidden" name="id" value="<?= $data['id'] ?>">
-                        <button onclick="return confirm('Apakah Anda yakin menghapus data ini?')" name="action" value="hapus" class="badge badge-danger">Hapus</button>
-                    </form>
+                    <?php if (isset($_SESSION['MEMBER'])) : ?>
+                        <?php $member = $_SESSION['MEMBER']; ?>
+
+                        <a href="index.php?page=form-edit-pegawai&id=<?= $data['id'] ?>" class="badge badge-warning">Edit</a>
+                        <?php if ($member['role'] != "staff") : ?>
+                            <form method="post" action="controllers/pegawaiController.php">
+                                <input type="hidden" name="id" value="<?= $data['id'] ?>">
+                                <button onclick="return confirm('Apakah Anda yakin menghapus data ini?')" name="action" value="hapus" class="badge badge-danger">Hapus</button>
+                            </form>
+                        <?php endif; ?>
+                    <?php endif; ?>
                 </td>
             </tr>
             <?php $no++; ?>
