f5a59211d2468c1d8658197b453541e6093c1d83
Rayhan <rayhan@badr.co.id>
2021-04-20 20:10:53 +0700

    filling about us content

--- about-us.php
+++ about-us.php
@@ -1,1 +1,43 @@
-ABOUT US
+<div class="row align-items-center">
+    <div class="col-sm-4 col-md-3">
+        <img class="img-fluid rounded-circle mb-4 mb-sm-0" src="assets/images/about-us/about-profile.jpg" alt="">
+    </div>
+    <div class="col-sm-8 col-md-9">
+        <p>Hello, I'm Rayhan, experienced Web Developer with a demonstrated history of working in the computer software industry. Skilled in Web Design, Web Application Development, Mobile App Development, UI/X Design. </p>
+    </div>
+</div>
+<hr class="my-4">
+<table class="table table-responsive">
+    <tr>
+        <td>Nama</td>
+        <td>:</td>
+        <td>Ahmed Rayhan Primadedas</td>
+    </tr>
+    <tr>
+        <td>NIM</td>
+        <td>:</td>
+        <td>0110291083</td>
+    </tr>
+    <tr>
+        <td>Kelas</td>
+        <td>:</td>
+        <td>TI Sore 2019 (TI 03)</td>
+    </tr>
+    <tr class="bg-dark">
+        <td class="text-white" colspan="3">Sosial Media</td>
+    </tr>
+    <tr>
+        <td>Linkedin</td>
+        <td>:</td>
+        <td>
+            <a href="https://www.linkedin.com/in/arayhan">https://www.linkedin.com/in/arayhan</a>
+        </td>
+    </tr>
+    <tr>
+        <td>Instagram</td>
+        <td>:</td>
+        <td>
+            <a href="https://www.instagram.com/arayhan_">https://www.instagram.com/arayhan_</a>
+        </td>
+    </tr>
+</table>
